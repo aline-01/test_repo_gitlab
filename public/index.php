17815ef41146b66eb503143cac5f3dfdecd69ef9
alien-01 <alien-01@alien.alien.net>
2023-06-06 20:13:02 -0400

    pushing this soruce to my first gitlab repo

--- public/index.php
+++ public/index.php
@@ -7,3 +7,11 @@ require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
+
+?>
+
+
+<h1>Hello world</h1>
+<script>
+   alert(1)
+</script>
